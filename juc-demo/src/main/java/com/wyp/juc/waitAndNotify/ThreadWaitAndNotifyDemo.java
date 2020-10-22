dc8e7ddf3db7ed58d6f03291b0c8243759bfbe2c
wyp <1834583115@qq.com>
2020-10-22 22:00:26 +0800

    修复多个生产者和多个消费者线程时虚假唤醒，导致过度生产或过度消费问题

--- juc-demo/src/main/java/com/wyp/juc/waitAndNotify/ThreadWaitAndNotifyDemo.java
+++ juc-demo/src/main/java/com/wyp/juc/waitAndNotify/ThreadWaitAndNotifyDemo.java
@@ -5,7 +5,7 @@ class MyData{
 
     public synchronized void increment() throws InterruptedException {
         //1、判断
-        if(num > 0){
+        while (num > 0){//多线程交互中判断条件使用if会存在虚假唤醒，需要使用while
             this.wait();
         }
         //2、 操作共享变量
@@ -17,7 +17,7 @@ class MyData{
 
     public synchronized void decrement() throws InterruptedException {
         //1、判断
-        if(num == 0){
+        while (num == 0){
             this.wait();
         }
         //2、 操作共享变量
@@ -34,30 +34,36 @@ class MyData{
  * A生产一个，B消费一个，B消费完后，A再生产
  * 实现交替,来1e轮,变量初始值为零。
  * 1    高内聚低耦合前提下，线程操作资源类
+ * 2    判断/操作共享变量/通知
+ * 3    多线程交互中，必须防止多线程的虚假唤醒，也即（判断只能用while，不能使用if）
  */
 public class ThreadWaitAndNotifyDemo {
 
     public static void main(String[] args) {
         MyData myData = new MyData();
-        new Thread(()->{
-            for (int i = 0; i < 10; i++) {
-                try {
-                    myData.increment();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        for (int k = 0; k < 3; k++) {
+            new Thread(()->{
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        myData.increment();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        },"A").start();
+            },"A"+k).start();
+        }
 
-        new Thread(()->{
-            for (int i = 0; i < 10; i++) {
-                try {
-                    myData.decrement();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        for (int k = 0; k < 3; k++) {
+            new Thread(()->{
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        myData.decrement();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        },"B").start();
+            },"B"+k).start();
+        }
 
     }
 
