593d107ec1a5853b01566390702a0e50e3dcedef
wyp <1834583115@qq.com>
2020-10-22 22:53:06 +0800

    使用lock和condition方式替代synchronize

---
+++ juc-demo/src/main/java/com/wyp/juc/waitAndNotify/improve/ThreadWaitAndNotifyDemo.java
@@ -0,0 +1,91 @@
+package com.wyp.juc.waitAndNotify.improve;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class MyData{
+    private int num = 0;
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+
+    public void increment() throws InterruptedException {
+        lock.lock();
+        try {
+            //1、判断
+            while (num > 0){//多线程交互中判断条件使用if会存在虚假唤醒，需要使用while
+                condition.await();// this.wait();
+            }
+            //2、 操作共享变量
+            num++;
+            System.out.println(Thread.currentThread().getName()+"\t"+num);
+            //3、通知其他线程
+            condition.signalAll();// this.notifyAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void decrement() throws InterruptedException {
+        lock.lock();
+        try {
+            //1、判断
+            while (num == 0){
+                condition.await();// this.wait();
+            }
+            //2、 操作共享变量
+            num--;
+            System.out.println(Thread.currentThread().getName()+"\t"+num);
+            //3、通知其他线程
+            condition.signalAll();// this.notifyAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+/**
+ * 题目：
+ * 现在两个线程，可以操作初始值为零的一个变量,实现一个线程对该变量加，一个线程对该变量减1,
+ * A生产一个，B消费一个，B消费完后，A再生产
+ * 实现交替,来1e轮,变量初始值为零。
+ * 1    高内聚低耦合前提下，线程操作资源类
+ * 2    判断/操作共享变量/通知
+ * 3    多线程交互中，必须防止多线程的虚假唤醒，也即（判断只能用while，不能使用if）
+ */
+public class ThreadWaitAndNotifyDemo {
+
+    public static void main(String[] args) {
+        MyData myData = new MyData();
+        for (int k = 0; k < 3; k++) {
+            new Thread(()->{
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        myData.increment();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },"A"+k).start();
+        }
+
+        for (int k = 0; k < 3; k++) {
+            new Thread(()->{
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        myData.decrement();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },"B"+k).start();
+        }
+
+    }
+
+}
