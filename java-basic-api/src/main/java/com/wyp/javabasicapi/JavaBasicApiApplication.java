b12e1ac4e094c451923e26f6d16c839c4b1721a3
wyupeng <wangyp@trgroup.cn>
2021-04-25 09:04:54 +0800

    java 基础API 的使用

---
+++ java-basic-api/src/main/java/com/wyp/javabasicapi/JavaBasicApiApplication.java
@@ -0,0 +1,13 @@
+package com.wyp.javabasicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBasicApiApplication.class, args);
+    }
+
+}
---
+++ java-basic-api/src/test/java/com/wyp/javabasicapi/JavaBasicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyp.javabasicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
