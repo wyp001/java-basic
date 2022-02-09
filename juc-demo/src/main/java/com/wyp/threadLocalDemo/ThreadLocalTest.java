116e0620915578670df48756de9f0b412b3ba671
wyupeng <wangyp@trgroup.cn>
2022-02-09 14:28:51 +0800

    使用threadLocal解决SimpleDateFormat多线程情况下出现线程安全问题

--- juc-demo/src/main/java/com/wyp/threadLocalDemo/ThreadLocalTest.java
+++ juc-demo/src/main/java/com/wyp/threadLocalDemo/ThreadLocalTest.java
@@ -1,6 +1,7 @@
 package com.wyp.threadLocalDemo;
 
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,24 +12,32 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 多线程情况下使用 SimpleDateFormat 进行日期格式化会存在线程安全问题
+ * 参考：项目中 SimpleDateFormat 的正确使用 https://www.cnblogs.com/christopherchan/p/11148859.html
  */
 public class ThreadLocalTest {
 
     /** 日期格式化类. */
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static ThreadLocal<DateFormat> threadLocal = new ThreadLocal<DateFormat>() {
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
 
     public static String formatDate(Date date) throws ParseException {
-        return sdf.format(date);
+        // return sdf.format(date);
+        return threadLocal.get().format(date);
     }
 
     public static Date parse(String strDate) throws ParseException {
-        return sdf.parse(strDate);
+        // return sdf.parse(strDate);
+        return threadLocal.get().parse(strDate);
     }
 
 
     public static void main(String[] args) throws InterruptedException {
         /** 单线程下测试. */
-        System.out.println(sdf.format(new Date()));
+        // System.out.println(sdf.format(new Date()));
 
         /** 多线程下测试. */
         ExecutorService service = Executors.newFixedThreadPool(100);
