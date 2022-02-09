4a793b661659d02d6f71c2650dfb61cf7037ea44
wyupeng <wangyp@trgroup.cn>
2022-02-09 14:21:53 +0800

    SimpleDateFormat多线程日期格式化存在线程安全问题

---
+++ juc-demo/src/main/java/com/wyp/threadLocalDemo/ThreadLocalTest.java
@@ -0,0 +1,52 @@
+package com.wyp.threadLocalDemo;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * 多线程情况下使用 SimpleDateFormat 进行日期格式化会存在线程安全问题
+ */
+public class ThreadLocalTest {
+
+    /** 日期格式化类. */
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static String formatDate(Date date) throws ParseException {
+        return sdf.format(date);
+    }
+
+    public static Date parse(String strDate) throws ParseException {
+        return sdf.parse(strDate);
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        /** 单线程下测试. */
+        System.out.println(sdf.format(new Date()));
+
+        /** 多线程下测试. */
+        ExecutorService service = Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 20; i++) {
+            service.execute(() -> {
+                for (int j = 0; j < 10; j++) {
+                    try {
+                        System.out.println(parse("2018-01-02 09:45:59"));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        // 等待上述的线程执行完后
+        //shutdown()关闭任务，awaitTermination关闭执行器（executor）。
+        service.shutdown();
+        service.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+}
