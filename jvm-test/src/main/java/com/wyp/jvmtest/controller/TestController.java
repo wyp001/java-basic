e8e84cca70cc6f7bd9cf5217fe0fc728265212be
wyupeng <wangyp@trgroup.cn>
2022-02-22 15:56:47 +0800

    arthas使用案例代码

--- jvm-test/src/main/java/com/wyp/jvmtest/controller/TestController.java
+++ jvm-test/src/main/java/com/wyp/jvmtest/controller/TestController.java
@@ -2,6 +2,7 @@ package com.wyp.jvmtest.controller;
 
 
 import com.wyp.jvmtest.case01.Math;
+import com.wyp.jvmtest.case02.ArthasDemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,24 @@ public class TestController {
         while (true) {
             math.compute();
         }
+    }
+
+    @GetMapping("/arthas/case1")
+    public void test02(){
+        // 模拟 CPU 过高
+        ArthasDemo.cpuHigh();
+    }
+
+    @GetMapping("/arthas/case2")
+    public void test03(){
+        // 模拟线程死锁
+        ArthasDemo.deadThread();
+    }
+
+    @GetMapping("/arthas/case3")
+    public void test04(){
+        // 不断的向 hashSet 集合增加数据
+        ArthasDemo.addHashSetThread();
     }
 
 }
